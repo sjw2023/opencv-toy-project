ac1eb28441994ddb4c13bd0eec0de348cfe85e65
JoowonSuh <sjw2023@gmail.com>
2024-05-31 09:38:10 +0900

    vision recognitions

--- FirstOpencvProgram.py
+++ FirstOpencvProgram.py
@@ -10,6 +10,9 @@ print('width : %d, height: %d' %(cap.get(3), cap.get(4)))
 # model
 model = YOLO("yolo-Weights/yolov8n.pt")
 
+# Train a model
+results = model.train(data=" /Users/joowon/Documents/GitHub/opencv-toy-project/Logistics.v10i.yolov8/data.yaml")
+
 # object classes
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
               "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
@@ -20,22 +23,75 @@ classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "trai
               "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
               "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
               "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-              "teddy bear", "hair drier", "toothbrush", "paper"]
+              "teddy bear", "hair drier", "toothbrush"]
+def markBox( img ):
+    # Load the image
+    # img = cv2.imread('box.jpg')
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-while(True):
-    ret, frame = cap.read()
+    edged = cv2.Canny(img, 19, 215)
+    # Apply adaptive threshold
+    thresh = cv2.adaptiveThreshold(edged, 255, 1, 1, 3, 2)
+    thresh_color = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
+
+    # apply some dilation and erosion to join the gaps - change iteration to detect more or less area's
+    thresh = cv2.dilate(thresh,None,iterations =1)
+    thresh = cv2.erode(thresh,None,iterations =1)
+
+    # Find the contours
+    contours,hierarchy = cv2.findContours(thresh,
+                                        cv2.RETR_TREE,
+                                        cv2.CHAIN_APPROX_SIMPLE)
+
+    # For each contour, find the bounding rectangle and draw it
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area >33000:
+            x,y,w,h = cv2.boundingRect(cnt)
+            cv2.rectangle(img,
+                        (x,y),(x+w,y+h),
+                        (0,255,0),
+                        5)
+
+    cv2.imshow('img',img)
+def markBox2(img):
+        # convert to grayscale
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    edged = cv2.Canny(img, 9, 99)
+    # Apply adaptive threshold
+    thresh = cv2.adaptiveThreshold(edged, 255, 1, 1, 3, 2)
+    thresh_color = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
+
+    # apply some dilation and erosion to join the gaps - change iteration to detect more or less area's
+    thresh = cv2.dilate(thresh,None,iterations =1)
+    thresh = cv2.erode(thresh,None,iterations =1)
+
+    # Find the contours
+    contours,hierarchy = cv2.findContours(thresh,
+                                        cv2.RETR_TREE,
+                                        cv2.CHAIN_APPROX_SIMPLE)
+
+    # For each contour, find the bounding rectangle and draw it
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area >33000:
+            x,y,w,h = cv2.boundingRect(cnt)
+            cv2.rectangle(img,
+                        (x,y),(x+w,y+h),
+                        (0,255,0),
+                        5)
+
+def basicScreenPrint(frame):
+    if(ret):
+        gray = cv2.cvtColor(frame, cv2.COLOR_BAYER_BG2GRAY)
+
+        cv2.imshow('frame_color', frame)
+        cv2.imshow('frame_gray', gray)
+def yoloClassNames():
+    print( model.names )
+def yoloRecognition(frame):
     results = model(frame, stream=True)
-
-
-    # if(ret):
-    #     # gray = cv2.cvtColor(frame, cv2.COLOR_BAYER_BG2GRAY)
-
-    #     cv2.imshow('frame_color', frame)
-    #     # cv2.imshow('frame_gray', gray)
-
-    #     if cv2.waitKey(1) == ord('q'):
-    #         break
-
     # coordinates
     for r in results:
         boxes = r.boxes
@@ -64,11 +120,20 @@ while(True):
             thickness = 2
 
             cv2.putText(frame, classNames[cls], org, font, fontScale, color, thickness)
-
+            
+while(True):
+    ret, frame = cap.read()
+    
+    # markBox2(frame)
+    yoloClassNames()
     cv2.imshow('Webcam', frame)
     if cv2.waitKey(1) == ord('q'):
         break
 
 cap.release()
 cv2.destroyAllWindows()
+
+
+
+
 
